9654deaa6ec4e2b64492db65e0b82bb88af5fa6d
lau <laura.nino.rosas@gmail.com>
2025-09-10 21:25:44 -0500

    Punto 1

--- Laboratorio4/Punto1.py
+++ Laboratorio4/Punto1.py
@@ -1,4 +1,13 @@
 lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-lista = lista[::-1]
-print(lista)
+pila = []
+
+for elemento in lista:
+    pila.append(elemento)
+
+lista_invertida = []
+
+while pila: # Mientras la pila no esté vacía
+    lista_invertida.append(pila.pop()) # Operación de "pop"
+
+print(lista_invertida)
